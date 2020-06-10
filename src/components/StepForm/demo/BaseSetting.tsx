74fb167b2d6d383e77f071c65acb72b3a40e0e1a
chenbingjie <chenbingjie@ghostcloud.cn>
2020-06-10 15:36:50 +0800

    优化

--- src/components/StepForm/demo/BaseSetting.tsx
+++ src/components/StepForm/demo/BaseSetting.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Form, Input, Popover, InputNumber } from 'antd';
 import { QuestionCircleOutlined } from '@ant-design/icons';
 import FieldMessage from './fieldMessage';
+import { FormRefContext } from '..';
 
 export interface IBaseSettingProps {
   dataWrapperName: string;
@@ -12,6 +13,7 @@ const BaseSetting: React.FC<IBaseSettingProps> = ({
   dataWrapperName,
   originData,
 }) => {
+  const formRef: any = useContext(FormRefContext);
   const itemLabel = (label: string, tipMsg?: string) => {
     if (tipMsg) {
       return (
@@ -32,12 +34,30 @@ const BaseSetting: React.FC<IBaseSettingProps> = ({
     return label;
   };
 
+  // 检查镜像和镜像tag的总长度
+  const imageTagValidator = (rule: any, value: string) => {
+    const values = formRef.current?.getFieldsValue();
+    let sumLen = Number(values[dataWrapperName].image.length + value.length);
+    if (!isNaN(sumLen) && sumLen > 254) {
+      return Promise.reject(new Error('镜像的完整名称长度不能超过254'));
+    }
+    return Promise.resolve();
+  };
+
   return (
     <>
       <Form.Item
         name={[`${dataWrapperName}`, 'applicationName']}
         initialValue={originData ? originData['applicationName'] : undefined}
-        rules={[{ required: true, message: '应用名不能为空' }]}
+        validateFirst
+        rules={[
+          { required: true, message: '应用名不能为空' },
+          { max: 245, message: '应用名长度最多245' },
+          {
+            pattern: /^[a-zA-Z][a-zA-Z0-9-]*$/,
+            message: '应用名只能以字母开头，内容为字母、中划线、数字',
+          },
+        ]}
         label={itemLabel('应用名', FieldMessage.applicationName)}
       >
         <Input placeholder='请输入应用名' allowClear />
@@ -45,7 +65,11 @@ const BaseSetting: React.FC<IBaseSettingProps> = ({
       <Form.Item
         name={[`${dataWrapperName}`, 'image']}
         initialValue={originData ? originData['image'] : undefined}
-        rules={[{ required: true, message: '镜像名不能为空' }]}
+        validateFirst
+        rules={[
+          { required: true, message: '镜像名不能为空' },
+          { max: 245, message: '镜像名长度最多245' },
+        ]}
         label={itemLabel('镜像名称', FieldMessage.image)}
       >
         <Input placeholder='请输入镜像名' allowClear />
@@ -53,7 +77,12 @@ const BaseSetting: React.FC<IBaseSettingProps> = ({
       <Form.Item
         name={[`${dataWrapperName}`, 'imageTag']}
         initialValue={originData ? originData['imageTag'] : undefined}
-        rules={[{ required: true, message: '镜像Tag不能为空' }]}
+        validateFirst
+        rules={[
+          { required: true, message: '镜像Tag不能为空' },
+          { max: 245, message: '镜像Tag长度最多245' },
+          { validator: imageTagValidator },
+        ]}
         label={itemLabel('镜像Tag', FieldMessage.imageTag)}
       >
         <Input placeholder='请输入镜像Tag' allowClear />
@@ -61,6 +90,7 @@ const BaseSetting: React.FC<IBaseSettingProps> = ({
       <Form.Item
         name={[`${dataWrapperName}`, 'cpuResource']}
         initialValue={originData ? originData['cpuResource'] : undefined}
+        validateFirst
         rules={[{ required: true, message: 'CPU核数不能为空' }]}
         label={itemLabel('CPU核数(毫核)', FieldMessage.cpuResource)}
       >
@@ -69,6 +99,7 @@ const BaseSetting: React.FC<IBaseSettingProps> = ({
       <Form.Item
         name={[`${dataWrapperName}`, 'memoryResource']}
         initialValue={originData ? originData['memoryResource'] : undefined}
+        validateFirst
         rules={[{ required: true, message: '内存容量不能为空' }]}
         label={itemLabel('内存容量(M)', FieldMessage.memoryResource)}
       >
